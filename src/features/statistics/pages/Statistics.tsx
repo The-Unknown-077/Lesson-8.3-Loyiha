9f76a15d560ac924a05c05b45f7d67a3e5c30f12
The-Unknown-077 <gamergamervich@gmail.com>
2025-09-22 16:25:40 +0500

    New

--- src/features/statistics/pages/Statistics.tsx
+++ src/features/statistics/pages/Statistics.tsx
@@ -1,5 +1,76 @@
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 
 const Statistics = () => {
+  const data = {
+    labels: [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    datasets: [
+      {
+        label: "Stats",
+        data: [500, 700, 900, 1200, 1300, 1800, 2000, 1200, 2700, 2000, 3200, 3500],
+        borderColor: "rgb(37, 99, 235)",
+        backgroundColor: "rgba(37, 99, 235, 0.2)",
+        tension: 0.4,
+        fill: true,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+        labels: {
+          color: "#374151",
+        },
+      },
+      title: {
+        display: false,
+      },
+    },
+    scales: {
+      x: {
+        ticks: { color: "#6B7280" },
+      },
+      y: {
+        ticks: { color: "#6B7280" },
+      },
+    },
+  };
   return (
     <div className="space-y-6">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
@@ -26,8 +97,8 @@ const Statistics = () => {
 
       <div className="bg-white rounded-2xl shadow p-6">
         <h2 className="text-lg font-semibold text-gray-700 mb-4">Monthly Overview</h2>
-        <div className="h-64 flex items-center justify-center text-gray-400">
-          Grafik uchun
+        <div className="h-64">
+          <Line data={data} options={options}/>
         </div>
       </div>
     </div>
